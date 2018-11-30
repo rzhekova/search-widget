f12d62c1178faa2c5b92e8b8f26b0e616b6ab64a
rzhekova <rzhekova@gmail.com>
2018-11-30 09:29:25 +0000

    adds comment to results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -25,6 +25,7 @@ class Results extends Component {
           const placeType = this.formatPlaceType(
             result.placeType
           ).toLowerCase();
+
           return result.name === "No results found" ? (
             <li className="no-results" key={result.name}>
               No results found
@@ -32,17 +33,26 @@ class Results extends Component {
           ) : (
             <li key={result.placeKey}>
               <span className={placeType}>
+                {/* this is the coloured rectangle which shows what type of place each result is */}
                 {this.formatPlaceType(result.placeType)}
               </span>
+
               <div>
                 <p className="result-name">
+                  {/* this is the name of the location */}
                   {result.name}
+
+                  {/* displays the three-letter airport code */}
                   {placeType === "airport" ? ` (${result.iata})` : null}
                 </p>
+
                 <p>
+                  {/* displays city if result is an airport or station */}
                   {placeType === "airport" || placeType === "station"
                     ? `${result.city}, `
                     : null}
+
+                  {/* displays region if city, statio or district */}
                   {placeType === "city" ||
                   placeType === "station" ||
                   placeType === "district"
@@ -50,6 +60,8 @@ class Results extends Component {
                       ? `${result.region}, `
                       : null
                     : null}
+
+                  {/* diplays country for every result */}
                   {result.country}
                 </p>
               </div>
@@ -70,7 +82,7 @@ class Results extends Component {
           results:
             searchTerm.length >= 2
               ? data.results.docs
-              : [{ name: "Not enough characters" }]
+              : [{ name: "Not enough characters" }] // setting the state with this message otherwise search terms with under two characters will still fetch results from the DB
         });
       })
       .catch(error => console.log(error));
